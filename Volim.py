3bd1d675e4fa5f728f9f3d19c068d9287bf1b7a9
poojakancherla <pknq6@mst.edu>
2017-03-15 23:29:10 -0500

    solved

---
+++ Volim.py
@@ -0,0 +1,18 @@
+K=int(input())
+N=int(input())
+
+
+time_left=210
+for i in range(N):
+    time,Z=input().split()
+    time_left=time_left-int(time)
+    if time_left<=0:
+        break
+    if Z=='N' or Z=='P':
+        continue
+    else:
+        if K==8:
+            K=1
+        else:
+            K+=1
+print(K)
