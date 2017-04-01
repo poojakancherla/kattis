01a0530bfaf9f179ed34d03502fb898447d566e2
Mohammed Jasam <mnqnd@mst.edu>
2017-04-01 01:59:55 -0500

    Update vauvau.py

--- vauvau.py
+++ vauvau.py
@@ -1,21 +1,35 @@
-for i in range(2,999):
-    dog1, dog1_peace, dog2, dog2_peace=map(int, input().split())
-    postman, milkman,garbageman=map(int, input().split())
-    if dog1>=postman and dog2>=postman:
-        print('both')
-    elif dog1>=postman or dog2>=postman:
-        print('one')
-    else:
-        print('none')
-    if dog1>=milkman and dog2>=milkman:
-        print('both')
-    elif dog1>=milkman or dog2>=milkman:
-        print('one')
-    else:
-        print('none')
-    if dog1>=garbageman and dog2>=garbageman:
-        print('both')
-    elif dog1>=garbageman or dog2>=garbageman:
-        print('one')
-    else:
-        print('none')
+dog1, dog1_peace, dog2, dog2_peace=map(int, input().split())
+postman, milkman,garbageman=map(int, input().split())
+
+d1=[]
+d2=[]
+for i in range(1000):
+    for x in range(dog1):
+        d1.append('a')
+    for x in range(dog1_peace):
+        d1.append('c')
+    for x in range(dog2):
+        d2.append('a')
+    for x in range(dog2_peace):
+        d2.append('c')
+
+if d1[postman - 1]=='a' and d2[postman - 1]=='a':
+    print('both')
+elif (d1[postman - 1]=='a' and d2[postman - 1]=='c') or  (d1[postman - 1]=='c' and d2[postman - 1]=='a'):
+    print('one')
+else:
+    print('none')
+
+if d1[milkman - 1]=='a' and d2[milkman - 1]=='a':
+    print('both')
+elif (d1[milkman - 1]=='a' and d2[milkman - 1]=='c') or  (d1[milkman - 1]=='c' and d2[milkman - 1]=='a'):
+    print('one')
+else:
+    print('none')
+
+if d1[garbageman - 1]=='a' and d2[garbageman - 1]=='a':
+    print('both')
+elif (d1[garbageman - 1]=='a' and d2[garbageman - 1]=='c') or  (d1[garbageman - 1]=='c' and d2[garbageman - 1]=='a'):
+    print('one')
+else:
+    print('none')
