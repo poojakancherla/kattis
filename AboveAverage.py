fd4036b4da5216fdf4d75814c3e781ac7e366ff9
poojakancherla <pknq6@mst.edu>
2017-12-10 00:22:33 -0600

    ..

---
+++ AboveAverage.py
@@ -0,0 +1,12 @@
+n = int(input())
+for i in range(n):
+    scores = [int(x) for x in input().split()]
+    num_students = scores[0]
+    scores = scores[1:]
+    avg = sum(scores)/(num_students)
+    count = 0
+    for i in scores:
+        if i > avg:
+            count +=1
+    above_avg = (count * 100)/(num_students)
+    print((str(round(above_avg, 3))) + "%")
