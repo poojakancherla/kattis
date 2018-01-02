9a121f7baf2c809a43d57923acc3ebfb4178bff3
poojakancherla <pknq6@mst.edu>
2018-01-02 14:38:26 -0600

    solving for the least possible solution

---
+++ 3D.py
@@ -0,0 +1,12 @@
+statues = int(input())
+printer = 1
+days = 0
+if statues == 1:
+    print(1)
+else:
+    while(printer < statues/2):
+        printer = printer * 2
+        days += 1
+
+
+    print(days + 2)
---
+++ ACMscoring.py
@@ -0,0 +1,2 @@
+while(true):
+    ip = input().split()
