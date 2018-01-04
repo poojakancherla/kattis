038adb863544e3681c0466be85a7583c9ee8dab3
pooja kancherla <pknq6@mst.edu>
2018-01-03 23:22:40 -0600

    solved

---
+++ prob3.py
@@ -0,0 +1,15 @@
+nums = [135678, 5678, 2345, 6453]
+nc = 0
+for i in nums:
+    count = 0
+    i = [int(x) for x in str(i)]
+    # print(i)
+    for j in range(len(i) - 1):
+        if i[j] < i[j+1]:
+            count += 1
+        else:
+            break
+    if count == len(i)-1:
+        nc += 1
+
+print(nc)
